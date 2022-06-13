03dbe80b9770cc483320a9d7e7ace8a1755e0675
radeveljic99 <80167674+radeveljic99@users.noreply.github.com>
2022-06-13 20:18:18 +0200

    Vuksan je debil

--- app/controllers/category.controller.js
+++ app/controllers/category.controller.js
@@ -3,150 +3,154 @@ const Category = require("../models/category.model.js");
 // Create and Save a new Category
 exports.create = (req, res) => {
 // Validate request
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    }
+	console.log('create called');
+	if (!req.body) {
+		res.status(400).send({
+			message: "Content can not be empty!"
+		});
+	}
 
-    // Create a Category
-    const category = new Category({
-        name: req.body.name
-    });
+	console.log('body = ', req.body);
 
-    // Save Category in the database
-    Category.create(category, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the category."
-            });
-        else res.send(data);
-    });
+	// Create a Category
+	const category = new Category({
+		name: req.body.name
+	});
+
+
+	// Save Category in the database
+	Category.create(category, (err, data) => {
+		if (err)
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while creating the category."
+			});
+		else res.send(data);
+	});
 };
 
 // Retrieve all Categorys from the database.
 exports.findAll = (req, res) => {
-    Category.getAll((err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving categories."
-            });
-        else res.send(data);
-    });
+	Category.getAll((err, data) => {
+		if (err)
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while retrieving categories."
+			});
+		else res.send(data);
+	});
 };
 
 exports.first = (req, res) => {
-    Category.firstCategory((err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving categories."
-            });
-        else res.send(data);
-    });
+	Category.firstCategory((err, data) => {
+		if (err)
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while retrieving categories."
+			});
+		else res.send(data);
+	});
 };
 
 
 // Find a single Category with a CategoryId
 exports.findOne = (req, res) => {
-    Category.findById(req.params.categoryId, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `Not found Category with id ${req.params.categoryId}.`
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error retrieving Category with id " + req.params.categoryId
-                });
-            }
-        } else res.send(data);
-    });
+	Category.findById(req.params.categoryId, (err, data) => {
+		if (err) {
+			if (err.kind === "not_found") {
+				res.status(404).send({
+					message: `Not found Category with id ${req.params.categoryId}.`
+				});
+			} else {
+				res.status(500).send({
+					message: "Error retrieving Category with id " + req.params.categoryId
+				});
+			}
+		} else res.send(data);
+	});
 };
 
 exports.findProducts = (req, res) => {
-    if (req.query.page && req.query.productsPerPage) {
-        Category.findPage(req.params.categoryId, req.query.page, req.query.productsPerPage, (err, data) => {
-            if (err) {
-                res.status(500).send({
-                    message: "Error retrieving products"
-                });
-            } else {
-                res.send(data);
-            }
-        })
-    } else {
-        Category.findAllProducts(req.params.categoryId, (err, data) => {
-            if (err) {
-                res.status(500).send({
-                    message: "Error retrieving products"
-                });
-            } else {
-                res.send(data);
-            }
-        });
-    }
+	if (req.query.page && req.query.productsPerPage) {
+		Category.findPage(req.params.categoryId, req.query.page, req.query.productsPerPage, (err, data) => {
+			if (err) {
+				res.status(500).send({
+					message: "Error retrieving products"
+				});
+			} else {
+				res.send(data);
+			}
+		})
+	} else {
+		Category.findAllProducts(req.params.categoryId, (err, data) => {
+			if (err) {
+				res.status(500).send({
+					message: "Error retrieving products"
+				});
+			} else {
+				res.send(data);
+			}
+		});
+	}
 
 }
 
 exports.countProducts = (req, res) => {
-    Category.countProducts(req.params.categoryId, (err, data) => {
-        if (err) {
-            res.status(500).send({
-                message: "Error retrieving products"
-            });
-        } else {
-            res.send(data);
-        }
-    });
+	Category.countProducts(req.params.categoryId, (err, data) => {
+		if (err) {
+			res.status(500).send({
+				message: "Error retrieving products"
+			});
+		} else {
+			res.send(data);
+		}
+	});
 }
 
 // Update a Category identified by the CategoryId in the request
 exports.update = (req, res) => {
-    console.log("Parametri", req.query.name);
-    // Validate Request
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    }
+	console.log("Parametri", req.query.name);
+	// Validate Request
+	if (!req.body) {
+		res.status(400).send({
+			message: "Content can not be empty!"
+		});
+	}
 
-    Category.updateById(
-        req.params.categoryId,
-        new Category(req.query),
-        (err, data) => {
-            if (err) {
-                if (err.kind === "not_found") {
-                    res.status(404).send({
-                        message: `Not found Category with id ${req.params.categoryId}.`
-                    });
-                } else {
-                    res.status(500).send({
-                        message: "Error updating Category with id " + req.params.categoryId
-                    });
-                }
-            } else res.send(data);
-        }
-    );
+	Category.updateById(
+		req.params.categoryId,
+		new Category(req.query),
+		(err, data) => {
+			if (err) {
+				if (err.kind === "not_found") {
+					res.status(404).send({
+						message: `Not found Category with id ${req.params.categoryId}.`
+					});
+				} else {
+					res.status(500).send({
+						message: "Error updating Category with id " + req.params.categoryId
+					});
+				}
+			} else res.send(data);
+		}
+	);
 };
 
 // Delete a Category with the specified CategoryId in the request
 exports.delete = (req, res) => {
-    Category.remove(req.params.categoryId, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `Not found Category with id ${req.params.categoryId}.`
-                });
-            } else {
-                res.status(500).send({
-                    message: "Could not delete Category with id " + req.params.categoryId
-                });
-            }
-        } else res.send({message: `Category was deleted successfully!`});
-    });
+	Category.remove(req.params.categoryId, (err, data) => {
+		if (err) {
+			if (err.kind === "not_found") {
+				res.status(404).send({
+					message: `Not found Category with id ${req.params.categoryId}.`
+				});
+			} else {
+				res.status(500).send({
+					message: "Could not delete Category with id " + req.params.categoryId
+				});
+			}
+		} else res.send({message: `Category was deleted successfully!`});
+	});
 };
 
 // Delete all Categorys from the database.
--- app/models/category.model.js
+++ app/models/category.model.js
@@ -1,154 +1,155 @@
 const sql = require('./db.js');
 
 const Category = function (category) {
-    this.name = category.name;
+	this.name = category.name;
 };
 
 Category.create = (newCategory, result) => {
-    sql.query("INSERT INTO category SET ? ", newCategory, (err, res) => {
-        if (err) {
-            console.log('error: ', err);
-            result(err, null);
-            return;
-        }
-        console.log("created category: ", {id: res.insertId, ...newCategory});
-        result(null, {id: res.insertId, ...newCategory});
-    });
+	sql.query("INSERT INTO category SET ? ", newCategory, (err, res) => {
+		if (err) {
+			console.log('error in create: ', err);
+			result(err, null);
+			return;
+		}
+		console.log("created category: ", {id: res.insertId, ...newCategory});
+		result(null, {id: res.insertId, ...newCategory});
+	});
 };
 
 Category.findById = (categoryId, result) => {
-    sql.query(`SELECT * FROM category WHERE id = ${categoryId}`, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(err, null);
-            return;
-        }
-        if (res.length) {
-            console.log("found category: ", res[0]);
-            result(null, res[0]);
-            return;
-        }
-        result({kind: "not_found"}, null);
-    });
+	sql.query(`SELECT * FROM category WHERE id = ${categoryId}`, (err, res) => {
+		if (err) {
+			console.log("error: ", err);
+			result(err, null);
+			return;
+		}
+		if (res.length) {
+			console.log("found category: ", res[0]);
+			result(null, res[0]);
+			return;
+		}
+		result({kind: "not_found"}, null);
+	});
 };
 
 Category.findPage = (categoryId, page, productsPerPage, result) => {
-    sql.query('SELECT * FROM product WHERE category_id = ?  LIMIT ? OFFSET ?',
-        [parseInt(categoryId), parseInt(productsPerPage), ( page - 1 ) * parseInt(productsPerPage)],
-        (err, res) => {
-            if (err) {
-                console.log("error: ", err);
-                result(err, null);
-                return;
-            }
-            if (res.length) {
-                console.log("found category: ", res);
-                result(null, res);
-                return;
-            }
-            result({kind: "not_found"}, null);
-        });
+	sql.query('SELECT * FROM product WHERE category_id = ?  LIMIT ? OFFSET ?',
+		[parseInt(categoryId), parseInt(productsPerPage), (page - 1) * parseInt(productsPerPage)],
+		(err, res) => {
+			if (err) {
+				result(err, null);
+				return;
+			}
+			if (res.length >= 0) {
+				result(null, res);
+				return;
+			}
+			result({kind: "not_found"}, null);
+		});
 }
 
 
 Category.findAllProducts = (categoryId, result) => {
-    sql.query("SELECT * FROM product WHERE category_id = ?", categoryId, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-        result(null, res);
-    });
+	console.log('query called');
+	sql.query("SELECT * FROM product WHERE category_id = ?", categoryId, (err, res) => {
+		console.log('vuksan');
+		console.log(res);
+		if (err) {
+			console.log("error: ", err);
+			result(null, err);
+			return;
+		}
+		result(null, res);
+	});
 };
 
 Category.getAll = result => {
-    sql.query("SELECT * FROM category", (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
+	sql.query("SELECT * FROM category", (err, res) => {
+		if (err) {
+			console.log("error: ", err);
+			result(null, err);
+			return;
+		}
 
-        console.log("categories: ", res);
-        result(null, res);
-    });
+		console.log("categories: ", res);
+		result(null, res);
+	});
 };
 
 Category.firstCategory = result => {
-    sql.query("SELECT * FROM category limit 1", (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-        console.log("categories: ", res);
-        result(null, res);
-    })
+	sql.query("SELECT * FROM category limit 1", (err, res) => {
+		if (err) {
+			console.log("error: ", err);
+			result(null, err);
+			return;
+		}
+		console.log("categories: ", res);
+		result(null, res);
+	})
 }
 
 Category.updateById = (id, category, result) => {
-    sql.query(
-        'UPDATE category SET name = ? WHERE id = ?',
-        [category.name, id],
-        (err, res) => {
-            if (err) {
-                console.log("error: ", err);
-                result(null, err);
-                return;
-            }
-            if (res.affectedRows == 0) {
-                result({kind: "not_found"}, null);
-                return;
-            }
-            console.log("updated category: ", {id: id, ...category});
-            result(null, {id: id, ...category});
-        }
-    );
+	sql.query(
+		'UPDATE category SET name = ? WHERE id = ?',
+		[category.name, id],
+		(err, res) => {
+			if (err) {
+				console.log("error: ", err);
+				result(null, err);
+				return;
+			}
+			if (res.affectedRows == 0) {
+				result({kind: "not_found"}, null);
+				return;
+			}
+			console.log("updated category: ", {id: id, ...category});
+			result(null, {id: id, ...category});
+		}
+	);
 };
 
 
 Category.remove = (id, result) => {
-    sql.query("DELETE FROM category WHERE id = ?", id, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
+	sql.query("DELETE FROM category WHERE id = ?", id, (err, res) => {
+		if (err) {
+			console.log("error: ", err);
+			result(null, err);
+			return;
+		}
 
-        if (res.affectedRows == 0) {
-            result({kind: "not_found"}, null);
-            return;
-        }
+		if (res.affectedRows == 0) {
+			result({kind: "not_found"}, null);
+			return;
+		}
 
-        console.log("deleted category with id: ", id);
-        result(null, res);
-    });
+		console.log("deleted category with id: ", id);
+		result(null, res);
+	});
 };
 
 Category.removeAll = result => {
-    sql.query("DELETE FROM category", (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
+	sql.query("DELETE FROM category", (err, res) => {
+		if (err) {
+			console.log("error: ", err);
+			result(null, err);
+			return;
+		}
 
-        console.log(`deleted ${res.affectedRows} categories`);
-        result(null, res);
-    });
+		console.log(`deleted ${res.affectedRows} categories`);
+		result(null, res);
+	});
 };
 
 Category.countProducts = (categoryId, result) => {
-    sql.query("SELECT COUNT(*) AS broj_proizvoda FROM product WHERE category_id =  ? " , categoryId , (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-        console.log("number of products : ", res[0]);
-        result(null, res[0]);
-    })
+	sql.query("SELECT COUNT(*) AS broj_proizvoda FROM product WHERE category_id =  ? ", categoryId, (err, res) => {
+		if (err) {
+			console.log("error: ", err);
+			result(null, err);
+			return;
+		}
+		console.log("number of products : ", res[0]);
+		result(null, res[0]);
+	})
 }
 
 module.exports = Category;
